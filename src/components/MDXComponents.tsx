3281e1afc32ea58d1516c0423b80560a43337437
Justin <57725347+juskek@users.noreply.github.com>
2023-12-22 22:28:07 +0800

    feat: create ImageWithCaption

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,13 +1,15 @@
-import TOCInline from 'pliny/ui/TOCInline'
-import Pre from 'pliny/ui/Pre'
-import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
+import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
+import Pre from 'pliny/ui/Pre'
+import TOCInline from 'pliny/ui/TOCInline'
 import Image from './Image'
+import ImageWithCaption from './ImageWithCaption'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 
 export const components: MDXComponents = {
   Image,
+  ImageWithCaption,
   TOCInline,
   a: CustomLink,
   pre: Pre,
