73da30a1e557b1bc705ec717f1ff801a3a8af33d
Justin Kek <justinkekjq@gmail.com>
2024-02-22 07:40:05 +0000

    feat: add privacy policy text

---
+++ src/app/eco-toss/privacy/page.tsx
@@ -0,0 +1,127 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1>Social Games Privacy Policy</h1>
+
+      <div>
+        <h2>The type of personal information we collect:</h2>
+        <ul>
+          <li>Name</li>
+          <li>Email Address</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>How we get the personal information and why we have it:</h2>
+        <p>
+          Most of the personal information we process is provided to us directly by you for one of
+          the following reasons:
+        </p>
+        <ul>
+          <li>
+            When you sign in to the app and use your real name as your display name, it is stored in
+            our servers.
+          </li>
+        </ul>
+        <p>
+          We receive personal information indirectly, from the following sources in the following
+          scenarios:
+        </p>
+        <ul>
+          <li>
+            When you sign in to the app using Continue with Google/Apple, we may receive your name
+            and email address from Google/Apple. However, this information is only made available to
+            us at the point of signing in, and it is not stored within our servers. We use the
+            information that you have given us in order to authenticate your account.
+          </li>
+        </ul>
+        <p>
+          Under the UK General Data Protection Regulation (UK GDPR), the lawful bases we rely on for
+          processing this information are:
+        </p>
+        <ul>
+          <li>
+            Your consent. You are able to remove your consent at any time. You can do this by
+            deleting your account by contacting{' '}
+            <a href="mailto:justin@kek.technology">justin@kek.technology</a>.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>How we store your personal information:</h2>
+        <p>Your information is not stored on our servers.</p>
+      </div>
+
+      <div>
+        <h2>Your data protection rights:</h2>
+        <p>Under data protection law, you have rights including:</p>
+        <ul>
+          <li>
+            Your right of access - You have the right to ask us for copies of your personal
+            information.
+          </li>
+          <li>
+            Your right to rectification - You have the right to ask us to rectify personal
+            information you think is inaccurate. You also have the right to ask us to complete
+            information you think is incomplete.
+          </li>
+          <li>
+            Your right to erasure - You have the right to ask us to erase your personal information
+            in certain circumstances.
+          </li>
+          <li>
+            Your right to restriction of processing - You have the right to ask us to restrict the
+            processing of your personal information in certain circumstances.
+          </li>
+          <li>
+            Your right to object to processing - You have the right to object to the processing of
+            your personal information in certain circumstances.
+          </li>
+          <li>
+            Your right to data portability - You have the right to ask that we transfer the personal
+            information you gave us to another organisation, or to you, in certain circumstances.
+          </li>
+        </ul>
+        <p>
+          You are not required to pay any charge for exercising your rights. If you make a request,
+          we have one month to respond to you.
+        </p>
+        <p>
+          Please contact us at [insert email address, phone number, and/or postal address] if you
+          wish to make a request.
+        </p>
+      </div>
+
+      <div>
+        <h2>How to complain:</h2>
+        <p>
+          If you have any concerns about our use of your personal information, you can make a
+          complaint to us at <a href="mailto:justin@kek.technology">justin@kek.technology</a>.
+        </p>
+        <p>You can also complain to the ICO if you are unhappy with how we have used your data.</p>
+        <p>
+          The ICO's address:
+          <br />
+          Information Commissioner's Office
+          <br />
+          Wycliffe House
+          <br />
+          Water Lane
+          <br />
+          Wilmslow
+          <br />
+          Cheshire
+          <br />
+          SK9 5AF
+          <br />
+        </p>
+        <p>
+          Helpline number: 0303 123 1113
+          <br />
+          ICO website: <a href="https://www.ico.org.uk">https://www.ico.org.uk</a>
+        </p>
+      </div>
+    </div>
+  )
+}
