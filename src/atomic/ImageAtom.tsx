ff7ec3fdf4fde31a246d065697ef19733f98a33c
Justin <57725347+juskek@users.noreply.github.com>
2024-03-08 17:41:21 +0000

    Feat/kek 2 3d flame (#16)
    * Revert "chore: delete tags"
    
    This reverts commit 19245eae6d6c78f7f81f13bffd2725116f327593.
    
    * hide: tags in nav bar
    
    * feat: allow setting maxSize on ImageAtom
    
    * article: eco toss pseudo 3d on 2d flame engine

--- src/atomic/ImageAtom.tsx
+++ src/atomic/ImageAtom.tsx
@@ -5,18 +5,34 @@ type Props = {
   altText?: string
   caption?: string
   source?: string
+  maxHeight?: string
+  maxWidth?: string
 }
 
-const ImageAtom = ({ src, altText, caption, source }: Props) => {
+const ImageAtom = ({ src, altText, caption, source, maxHeight, maxWidth }: Props) => {
   const showCaption = caption || source
   return (
-    <figure style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <figure
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        overflow: 'hidden',
+      }}
+    >
       <Image
         src={src}
         alt={altText ?? caption ?? ''}
         height={500}
         width={500}
-        style={{ width: '100%', height: '100%', position: 'relative' }}
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'relative',
+          maxWidth, // Set max width for the image
+          maxHeight, // Set max height for the image
+        }}
+        objectFit="contain"
       />
       {showCaption && (
         <figcaption style={{ textAlign: 'center', fontSize: 'smaller' }}>
