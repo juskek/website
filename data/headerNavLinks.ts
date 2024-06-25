30ba57fde85c4b24d4a9e643021089df324452f0
Justin Kek <justinkekjq@gmail.com>
2024-06-25 12:00:51 +0100

    add music page with spotify embed

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  // { href: '/tags', title: 'Tags' },
+  { href: '/music', title: 'Music' },
   // { href: '/projects', title: 'Projects' },
 ]
 
--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src * https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com;
   font-src 'self';
+  frame-src https://open.spotify.com;
   frame-ancestors 'self';
 `
 
