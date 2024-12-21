ae10f0152c07f2435be1646ae75b8f131249efc5
Justin Kek <justinkekjq@gmail.com>
2024-12-21 22:14:34 +0000

    feat: make canvas fill page

--- src/app/private/ujo-uvsot-24/page.tsx
+++ src/app/private/ujo-uvsot-24/page.tsx
@@ -26,12 +26,20 @@ function Box(props: ThreeElements['mesh']) {
 
 export default function Page() {
   return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
+    <div className="h-screen bg-black">
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </div>
   )
 }
