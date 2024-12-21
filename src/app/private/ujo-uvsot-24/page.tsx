10c7e9c756ec1086e95b196891d5fe61e2b9e9b6
Justin Kek <justinkekjq@gmail.com>
2024-12-21 23:09:55 +0000

    feat: loading indicator

--- src/app/private/ujo-uvsot-24/page.tsx
+++ src/app/private/ujo-uvsot-24/page.tsx
@@ -2,27 +2,30 @@
 
 import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { MeshStandardMaterial, Object3D } from 'three'
+import { TeaLoadingIndicator } from './TeaLoadingIndicator'
 
 const rotationSpeed = 0.01
 
-function ModelWithTexture() {
-  const { scene, materials } = useGLTF('/static/models/peony/model.glb')
+function ModelWithTexture({ setLoading }) {
+  const { scene, materials } = useGLTF('/static/models/peony/model.glb', true) // Enable loading state
   const texture = useTexture('/static/models/peony/texture.jpeg')
-
   const modelRef = useRef<Object3D>(null)
 
   useEffect(() => {
     if (materials && texture) {
-      console.log({ materials })
       const material = materials['']
       if (material && material instanceof MeshStandardMaterial) {
         material.map = texture
         material.needsUpdate = true
       }
     }
-  }, [materials, texture])
+
+    if (scene) {
+      setLoading(false) // Model has finished loading
+    }
+  }, [materials, texture, scene, setLoading])
 
   useFrame(() => {
     if (modelRef.current) {
@@ -34,8 +37,15 @@ function ModelWithTexture() {
 }
 
 export default function HomePage() {
+  const [loading, setLoading] = useState(true)
+
   return (
     <div className="h-screen w-screen">
+      {loading && (
+        <div className="absolute inset-0 flex items-center justify-center">
+          <TeaLoadingIndicator />
+        </div>
+      )}
       <Canvas
         camera={{
           position: [0, 2, 5],
@@ -44,7 +54,7 @@ export default function HomePage() {
       >
         <ambientLight intensity={3} />
         <directionalLight position={[5, 5, 5]} />
-        <ModelWithTexture />
+        <ModelWithTexture setLoading={setLoading} />
         <OrbitControls />
       </Canvas>
     </div>
