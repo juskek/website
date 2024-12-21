3a1da1d8b751768f811044b32bb3c64b62723284
Justin Kek <justinkekjq@gmail.com>
2024-12-21 22:11:59 +0000

    poc: basic three fiber example

--- src/app/private/ujo-uvsot-24/page.tsx
+++ src/app/private/ujo-uvsot-24/page.tsx
@@ -1,3 +1,37 @@
+'use client' // Ensures this page renders on the client
+
+import { Canvas, ThreeElements, useFrame } from '@react-three/fiber'
+import { useRef, useState } from 'react'
+import * as THREE from 'three'
+
+function Box(props: ThreeElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
 export default function Page() {
-  return <div className="mx-auto max-w-4xl px-4 py-8"></div>
+  return (
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
 }
