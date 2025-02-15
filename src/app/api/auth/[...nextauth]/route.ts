82ada43aed303a319a940d4e3442d06544482d91
Justin Kek <justinkekjq@gmail.com>
2025-02-15 12:37:43 +0000

    feat: route-specific passwords
    sq

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,23 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
+const pagePasswords: Record<string, string> = {
+  '/private/ujo-uvsot-24': 'chagi',
+  '/blog/algorithmic-trading/*': 'stonks',
+}
+
+const getPasswordForRoute = (requestUrl: string): string | null => {
+  for (const [route, password] of Object.entries(pagePasswords)) {
+    if (route.includes('*') && requestUrl.startsWith(route.replace('/*', ''))) {
+      return password
+    }
+    if (requestUrl.includes(route)) {
+      return password
+    }
+  }
+  return null
+}
+
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
@@ -9,7 +26,20 @@ const handler = NextAuth({
         password: { label: '', type: 'password' },
       },
       async authorize(credentials, req) {
-        if (credentials?.password === 'chagi') {
+        const callbackUrl =
+          new URL(req.headers?.referer || '').searchParams.get('callbackUrl') || null
+
+        if (callbackUrl === null) {
+          return null
+        }
+
+        const expectedPassword = getPasswordForRoute(callbackUrl)
+
+        if (expectedPassword === null) {
+          return null
+        }
+
+        if (credentials?.password === expectedPassword) {
           const user = { id: '1', name: 'User' }
           return user
         }
