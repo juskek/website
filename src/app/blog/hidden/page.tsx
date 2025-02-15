fa651c57ee5e8bef6b91b6f82302735bac80135c
Justin Kek <justinkekjq@gmail.com>
2025-02-15 20:08:50 +0000

    feat: show hidden articles

---
+++ src/app/blog/hidden/page.tsx
@@ -0,0 +1,33 @@
+import ListLayout from '@/layouts/ListLayoutWithTags'
+import { allBlogs } from 'contentlayer/generated'
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+import { genPageMetadata } from 'src/app/seo'
+
+const POSTS_PER_PAGE = 5
+
+export const metadata = genPageMetadata({ title: 'Blog' })
+
+export default function BlogPage() {
+  const sortedPosts = sortPosts(allBlogs)
+  const hiddenPosts = sortedPosts.filter((blog) => blog.hidden === true)
+  const posts = allCoreContent(hiddenPosts)
+
+  const pageNumber = 1
+  const initialDisplayPosts = posts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+  }
+
+  return (
+    <ListLayout
+      posts={posts}
+      initialDisplayPosts={initialDisplayPosts}
+      pagination={pagination}
+      title="All Posts"
+    />
+  )
+}
