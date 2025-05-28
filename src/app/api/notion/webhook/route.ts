5b60ad4ad10243a9e2aaa665f504e8cd0568d99d
Justin Kek <justinkekjq@gmail.com>
2025-05-29 00:15:08 +0100

    feat: detect change to Baking Event x Recipe table

--- src/app/api/notion/webhook/route.ts
+++ src/app/api/notion/webhook/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+const BAKING_EVENTS_RECIPE_TABLE_ID = '2018f264-e43e-80f6-b3e0-f24ecbb9e565'
+
 export async function POST(req: NextRequest) {
   console.log('Received POST request to /api/notion/webhook')
   const body = await req.json()
@@ -10,6 +12,12 @@ export async function POST(req: NextRequest) {
   }
 
   console.log('Received webhook:', body)
+
+  const isBakingEventsRecipesTableEvent =
+    body.data.parent.type === 'database' && body.data.parent.id === BAKING_EVENTS_RECIPE_TABLE_ID
+  if (isBakingEventsRecipesTableEvent) {
+    console.log('Baking Event Recipe Table updated, entity id:', body.entity.id)
+  }
 
   return NextResponse.json({ received: true })
 }
