76feeb1f8166148fa3361f1847184b8a9c5882e3
Justin Kek <justinkekjq@gmail.com>
2025-05-29 13:30:02 +0100

    fix: A "use server" file can only export async functions, found object

--- src/lib/notion/notionClient.ts
+++ src/lib/notion/notionClient.ts
@@ -1,6 +1,8 @@
-'use server'
-
 import { Client } from '@notionhq/client'
+
+if (typeof window !== 'undefined') {
+  throw new Error('Notion client must only be used server-side')
+}
 
 export const notionClient = new Client({
   auth: process.env.NOTION_API_KEY,
